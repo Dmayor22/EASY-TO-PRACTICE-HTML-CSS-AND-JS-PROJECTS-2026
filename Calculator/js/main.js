3e385f416f0174ea4b71d8353d64a1ec25ce8a2d
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-08-26 07:09:49 +0100

    calculator app

--- Calculator/js/main.js
+++ Calculator/js/main.js
@@ -1,1 +1,18 @@
 // variables
+const displayValues = document.querySelector(".display");
+const btns = document.querySelectorAll(".numerical button");
+const equalTo = document.querySelector("#equal");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const calValue = (displayValues.textContent += btn.textContent);
+
+    if (btn.textContent === "AC") {
+      displayValues.textContent = "";
+    }
+    if (btn.textContent === "DEL") {
+      displayValues.textContent = calValue.slice(0, -4);
+    }
+  });
+});
+
