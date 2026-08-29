9dbb124369cfeb68556f0df7c1ac493b691542e0
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-08-29 19:26:38 +0100

    coupon code generator

---
+++ Coupon code generator/js/main.js
@@ -0,0 +1,14 @@
+// variables
+const date = document.getElementById("date");
+const copyBtn = document.getElementById("coupon_copycode");
+const couponCode = document.getElementById("coupon_code");
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(couponCode.innerHTML);
+
+  copyBtn.innerHTML = "COPIED";
+
+  setTimeout(() => {
+    copyBtn.innerHTML = "COPY CODE";
+  }, 3000);
+});
